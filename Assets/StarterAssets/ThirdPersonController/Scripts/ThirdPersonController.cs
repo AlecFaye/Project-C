eaa9a580507fa2f9a299d0ea4985dea217a5879c
BiggyBruhmanz <habibkhanu45@gmail.com>
2022-11-29 23:05:01 -0500

    found problem (Look at why 2nd player/client is controlled by controller)

--- Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -135,6 +135,8 @@ namespace StarterAssets
 
         private void Start()
         {
+            //if (!IsOwner) return; // Checks if player is NOT owner of the server
+            
             _cinemachineTargetYaw = CinemachineCameraTarget.transform.rotation.eulerAngles.y;
             
             _hasAnimator = TryGetComponent(out _animator);
@@ -223,7 +225,10 @@ namespace StarterAssets
 
             // note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
             // if there is no input, set the target speed to 0
-            if (_input.move == Vector2.zero) targetSpeed = 0.0f;
+            if (_input.move == Vector2.zero) {
+                targetSpeed = 0.0f;
+                //Debug.Log(_input);
+            }
 
             // a reference to the players current horizontal velocity
             float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
