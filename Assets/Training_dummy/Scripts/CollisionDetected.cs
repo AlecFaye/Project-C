dd8fdeb53d7722d44e91315ea04ee5ae5b63f852
BiggyBruhmanz <habibkhanu45@gmail.com>
2022-12-06 16:56:12 -0500

    added base pickaxe

--- Assets/Training_dummy/Scripts/CollisionDetected.cs
+++ Assets/Training_dummy/Scripts/CollisionDetected.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class CollisionDetected : MonoBehaviour
 {
+    [SerializeField] private Weapon pfWeapon;
+    //[SerializeField] private Weapon.WeaponType weaponType = Weapon.WeaponType;
+    
+
     [SerializeField] private Animator animator;
     [SerializeField] private Enemy enemy;
 
     // Triggers when CollisionDetection script detects the enemy 
     public void Hit(float damageDealt, Weapon.WeaponType weaponType) {
         Debug.Log("Damage Value: " + damageDealt + " | Weapon Type: " + weaponType);
-        if (enemy) 
+        if (enemy)
             enemy.TakeDamage(damageDealt, weaponType);
 
         if (animator)
