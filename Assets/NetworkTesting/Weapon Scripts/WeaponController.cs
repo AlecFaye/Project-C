e9936f67cff00a4fe2caa2dbb35d89034239df1e
BiggyBruhmanz <habibkhanu45@gmail.com>
2022-12-06 19:42:24 -0500

    Fixed multiple Weapons and Added Pickaxe

--- Assets/NetworkTesting/Weapon Scripts/WeaponController.cs
+++ Assets/NetworkTesting/Weapon Scripts/WeaponController.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using StarterAssets;
+using QFSW.QC;
+using Unity.VisualScripting;
 
 
 //namespace WeaponController{
-    public class WeaponController : MonoBehaviour
+public class WeaponController : MonoBehaviour
     {
         public ThirdPersonController player; // Refrences the player it's attatched too
-                
+
+        [SerializeField] private Transform[] Hotbar;
+
+        private int selectedWeapon = 0;
+
         // Player Attack Stats
         [Header("Attack")]
         [Tooltip("If the character can Attack or not.")]
@@ -26,7 +32,40 @@ using StarterAssets;
         public List<Collider> enemiesHitList = new List<Collider>(); // Makes a list to keep track of which enemies were hit (enemies added by the CollisionDetection Script on weapons)
 
 
-        private IEnumerator OnAttack() 
+    private void Start()
+    {
+        foreach (Transform weapon in Hotbar)
+        {
+            if  (weapon != null) {
+                Transform tempWeapon = Instantiate(weapon, Vector3.zero, Quaternion.identity);
+                tempWeapon.transform.SetParent(this.transform);
+                tempWeapon.transform.localPosition = new Vector3(0.04f, -0.1f, 0.2f);
+                Debug.Log("Weapon Spawn Rotation: " + tempWeapon.transform.rotation);
+                tempWeapon.transform.localRotation = Quaternion.Euler(0f, 180f, 90f);
+                Debug.Log("Weapon Post Rotation: " + tempWeapon.transform.rotation);
+                Debug.Log("Weapon Post Rotation X: " + tempWeapon.transform.rotation.x);
+                Debug.Log("Weapon Post Rotation Y: " + tempWeapon.transform.rotation.y);
+                Debug.Log("Weapon Post Rotation Z: " + tempWeapon.transform.rotation.z);
+            }
+        }
+
+        SelectWeapon();
+    }
+
+    private void SelectWeapon()
+    {
+        int i = 0;
+        foreach (Transform weapon in transform)
+        {
+            if (i == selectedWeapon)
+                weapon.gameObject.SetActive(true);
+            else 
+                weapon.gameObject.SetActive(false);
+            i++;
+        }
+    }
+
+    private IEnumerator OnAttack() 
         {
             if (CanAttack && !IsAttacking && player.Grounded && player.IsOwner){
                 CanAttack = false;
@@ -41,11 +80,22 @@ using StarterAssets;
             }
         }
 
-        private void OnHotbarOne()
+        private void OnHotbar1()
         {
-            Debug.Log("Yeet One");
+            if (player.IsOwner){
+                Debug.Log("Yeet 1");
+                selectedWeapon = 0;
+                SelectWeapon();
+            }
         }
 
+        private void OnHotbar2()
+        {
+            if (player.IsOwner){
+                Debug.Log("Yeet 2");
+            selectedWeapon = 1;
+                SelectWeapon();
+            }
+        }
 
     }
-//}
