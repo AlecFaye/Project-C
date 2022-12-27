b0aa60dacf4eba375d3403c26c725255d88e34a6
BiggyBruhmanz <habibkhanu45@gmail.com>
2022-12-26 23:41:13 -0500

    Timer Done

---
+++ Assets/Habib Files/Game Timer and Score/GameTimer.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameTimer : MonoBehaviour
+{
+    private TextMeshProUGUI timerText;
+
+    private float timeValue = 3559f;
+    
+    public bool IsTimerStarted = false;
+
+
+    private void Awake() {
+        timerText = GetComponent<TextMeshProUGUI>();
+        DisplayTime();
+    }
+
+    private void Update() {
+        if (IsTimerStarted == false) return;
+
+        UpdateTime();
+        DisplayTime();
+    }
+
+    private void UpdateTime() { timeValue += Time.deltaTime; }
+
+    private void DisplayTime() {
+        float seconds = Mathf.FloorToInt(timeValue % 60);
+        float minutes = Mathf.FloorToInt((timeValue / 60) % 60);
+        float hours = Mathf.FloorToInt(timeValue / 3600);
+
+        timerText.text = string.Format("{0:00}:{1:00}:{2:00}", hours, minutes, seconds);
+    }
+}
